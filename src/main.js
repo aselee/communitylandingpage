15430b919823982685034f6cecf6d04808a12499
Andrew <32229265+aselee@users.noreply.github.com>
2018-10-24 02:24:31 -0500

    importing vuetify

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// using vuetify - DevLee
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+//
+
+
 Vue.config.productionTip = false
 
 new Vue({
