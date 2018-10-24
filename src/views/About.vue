c3bc694bdf75eb6371a0255c2d93c8b006f9e67f
Andrew <32229265+aselee@users.noreply.github.com>
2018-10-24 01:20:13 -0500

    initial 2.0

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,14 +4,6 @@
     <div class="title">
       <h1>{{title}}</h1>
     </div>
-    <div class="card">
-      <app-members></app-members>
-      <div>DevLee</div>
-      <div>MattL019</div>
-      <div>Rejam</div>
-      <div>Banhbaovy</div>
-      <div>Novajumper</div>
-    </div>
   </div>
 
 </template>
@@ -20,6 +12,7 @@
 <script>
 
 export default {
+
   data: function() {
     return {
       title: 'About Us'
